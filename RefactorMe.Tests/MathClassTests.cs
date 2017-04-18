6b283ca40d99e8df3f2b16ea89f4bddaefcd8ac6
Travis Williams <twilliams@datis.com>
2017-04-18 14:34:44 -0400

    case 4

--- RefactorMe.Tests/MathClassTests.cs
+++ RefactorMe.Tests/MathClassTests.cs
@@ -17,11 +17,11 @@ namespace RefactorMe
          KeyValuePair<string, double> expected = new KeyValuePair<string, double>(s, r);
 
          //Act
-         var testResult = MathClass.DoSomeMaths(7, 3, MathsType.Type1);
+         var testResult = MathClass.DoSomeMaths(a, b, MathsType.Type1);
 
          //Assert
-         Assert.Equal(testResult.Value.Key, expected.Key);
-         Assert.Equal(testResult.Value.Value, expected.Value);
+         Assert.Equal(expected.Key, testResult.Value.Key);
+         Assert.Equal(expected.Value, testResult.Value.Value);
       }
       [Fact]
       public void TestMathsType2()
@@ -29,34 +29,53 @@ namespace RefactorMe
          //Arrange
          var a = 2;
          var b = 9;
-         var s = "Starting\nSubtracted b from a\nMultiplied the result by b\nMultiplied the result by b again\nDivided the result by 3 times a\nSubtracted one-fifth of a from the result\nRounded to 2 decimals";
-         var r = 0.31;
+         var s = "Starting\nAdded 2 to a\nDivided the result by the difference of a and half of b\nMultiplied the result by 4 minus a\nAdded b to result\nMultiplied the result by 4 minus a\nDivided the result by the difference of a and half of b\nAdded b to result\nRounded to 2 decimals";
+         var r = 4.36;
          KeyValuePair<string, double> expected = new KeyValuePair<string, double>(s, r);
 
          //Act
-         var testResult = MathClass.DoSomeMaths(7, 3, MathsType.Type2);
+         var testResult = MathClass.DoSomeMaths(a, b, MathsType.Type2);
 
          //Assert
-         Assert.Equal(testResult.Value.Key, expected.Key);
-         Assert.Equal(testResult.Value.Value, expected.Value);
+         Assert.Equal(expected.Key, testResult.Value.Key);
+         Assert.Equal(expected.Value, testResult.Value.Value);
       }
+      [Fact]
       public void TestMathsType3()
       {
          //Arrange
-         var a = 2;
-         var b = 9;
-         var s = "Starting\nSubtracted b from a\nMultiplied the result by b\nMultiplied the result by b again\nDivided the result by 3 times a\nSubtracted one-fifth of a from the result\nRounded to 2 decimals";
-         var r = 0.31;
+         var a = 7;
+         var b = 13;
+         var s = "Starting\nAdded 2 to a\nAdded b to result\nDivided the result by the difference of a and half of b\nMultiplied the result by 4 minus a\nAdded b to result\nDivided the result by the difference of a and half of b\nMultiplied the result by 4 minus a\nAdded b to result\nDivided the result by the difference of a and half of b\nMultiplied the result by 4 minus a\nAdded b to result\nDivided the result by the difference of a and half of b\nMultiplied the result by 4 minus a\nAdded b to result\nRounded to 2 decimals";
+         var r = 26107;
          KeyValuePair<string, double> expected = new KeyValuePair<string, double>(s, r);
 
          //Act
-         var testResult = MathClass.DoSomeMaths(7, 3, MathsType.Type3);
+         var testResult = MathClass.DoSomeMaths(a, b, MathsType.Type3);
+
+         //Assert
+         Assert.Equal(expected.Key, testResult.Value.Key);
+         Assert.Equal(expected.Value, testResult.Value.Value);
+      }
+      [Fact]
+      public void TestMathsType4()
+      {
+         //Arrange
+         var a = 11;
+         var b = 2;
+         var s = "Starting\nAdded 2 to a\nMultiplied the result by 4 minus a\nAdded b to result\nDivided the result by the difference of a and half of b\nMultiplied the result by 4 minus a\nAdded b to result\nDivided the result by the difference of a and half of b\nAdded b to result\nAdded b to result\nDivided the result by the difference of a and half of b\nMultiplied the result by 4 minus a\nAdded b to result\nDivided the result by the difference of a and half of b\nMultiplied the result by 4 minus a\nAdded b to result\nDivided the result by the difference of a and half of b\nAdded b to result\nAdded b to result\nDivided the result by the difference of a and half of b\nMultiplied the result by 4 minus a\nRounded to 2 decimals";
+         var r = -3.2;
+         KeyValuePair<string, double> expected = new KeyValuePair<string, double>(s, r);
+
+         //Act
+         var testResult = MathClass.DoSomeMaths(a, b, MathsType.Type4);
+         Console.WriteLine("Test 4");
          Console.WriteLine(testResult.Value.Key);
          Console.WriteLine(testResult.Value.Value);
 
          //Assert
-         Assert.Equal(testResult.Value.Key, expected.Key);
-         Assert.Equal(testResult.Value.Value, expected.Value);
+         Assert.Equal(expected.Key, testResult.Value.Key);
+         Assert.Equal(expected.Value, testResult.Value.Value);
       }
    }
 }
--- RefactorMe/MathClass.cs
+++ RefactorMe/MathClass.cs
@@ -3,95 +3,155 @@ using System.Collections.Generic;
 
 namespace RefactorMe
 {
-   public enum MathsType
-   {
-      Type1,
-      Type2,
-      Type3,
-      Type4
-   }
+    public enum MathsType
+    {
+        Type1,
+        Type2,
+        Type3,
+        Type4
+    }
 
-   public static class MathClass
-   {
-      public static KeyValuePair<string, double>? DoSomeMaths(int a, int b, MathsType type)
-      {
-         if (type == MathsType.Type1)
-         {
-            var r = 0.0d;
-            var s = "Starting";
+    public static class MathClass
+    {
+        public static KeyValuePair<string, double>? DoSomeMaths(int a, int b, MathsType type)
+        {
+            if (type == MathsType.Type1)
+            {
+                var r = 0.0d;
+                var s = "Starting";
 
-            r = a + 2;
-            s += "\nAdded 2 to a";
-            r += b;
-            s += "\nAdded b to result";
-            r *= 4 - a;
-            s += "\nMultiplied the result by 4 minus a";
-            r /= a - .5 * b;
-            s += "\nDivided the result by the difference of a and half of b";
-            r = Math.Round(r, 2);
-            s += "\nRounded to 2 decimals";
+                r = a + 2;
+                s += "\nAdded 2 to a";
+                r += b;
+                s += "\nAdded b to result";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r = Math.Round(r, 2);
+                s += "\nRounded to 2 decimals";
 
-            var rv = new KeyValuePair<string, double>(s, r);
-            return rv;
-         }
-         else if (type == MathsType.Type2)
-         {
-            var r = 0.0d;
-            var s = "Starting";
+                var rv = new KeyValuePair<string, double>(s, r);
+                return rv;
+            }
+            else if (type == MathsType.Type2)
+            {
+                var r = 0.0d;
+                var s = "Starting";
 
-            r = a - b;
-            s += "\nSubtracted b from a";
-            r *= b;
-            s += "\nMultiplied the result by b";
-            r *= b;
-            s += "\nMultiplied the result by b again";
-            r /= 3 * a;
-            s += "\nDivided the result by 3 times a";
-            r -= .2 * a;
-            s += "\nSubtracted one-fifth of a from the result";
-            r = Math.Round(r, 2);
-            s += "\nRounded to 2 decimals";
+                r = a + 2;
+                s += "\nAdded 2 to a";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r += b;
+                s += "\nAdded b to result";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r += b;
+                s += "\nAdded b to result";
+                r = Math.Round(r, 2);
+                s += "\nRounded to 2 decimals";
 
-            var rv = new KeyValuePair<string, double>(s, r);
-            return rv;
-         }
-         else if (type == MathsType.Type3)
-         {
-            var r = 0.0d;
-            var s = "Starting";
+                var rv = new KeyValuePair<string, double>(s, r);
+                return rv;
+            }
+            else if (type == MathsType.Type3)
+            {
+                var r = 0.0d;
+                var s = "Starting";
 
-            r = a + 2;
-            s += "\nAdded 2 to a";
-            r += b;
-            s += "\nAdded b to result";
-            r *= 4 - a;
-            s += "\nMultiplied the result by 4 minus a";
-            r /= a - .5 * b;
-            s += "\nDivided the result by the difference of a and half of b";
+                r = a + 2;
+                s += "\nAdded 2 to a";
+                r += b;
+                s += "\nAdded b to result";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r += b;
+                s += "\nAdded b to result";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r += b;
+                s += "\nAdded b to result";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r += b;
+                s += "\nAdded b to result";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r += b;
+                s += "\nAdded b to result";
+                r = Math.Round(r, 2);
+                s += "\nRounded to 2 decimals";
 
-            var rv = new KeyValuePair<string, double>(s, r);
-            return rv;
-         }
-         else if (type == MathsType.Type4)
-         {
-            var r = 0.0d;
-            var s = "Starting";
+                var rv = new KeyValuePair<string, double>(s, r);
+                return rv;
+            }
+            else if (type == MathsType.Type4)
+            {
+                var r = 0.0d;
+                var s = "Starting";
 
-            r = a + 2;
-            s += "\nAdded 2 to a";
-            r += b;
-            s += "\nAdded b to result";
-            r *= 4 - a;
-            s += "\nMultiplied the result by 4 minus a";
-            r /= a - .5 * b;
-            s += "\nDivided the result by the difference of a and half of b";
+                r = a + 2;
+                s += "\nAdded 2 to a";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r += b;
+                s += "\nAdded b to result";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r += b;
+                s += "\nAdded b to result";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r += b;
+                s += "\nAdded b to result";
+                r += b;
+                s += "\nAdded b to result";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r += b;
+                s += "\nAdded b to result";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r += b;
+                s += "\nAdded b to result";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r += b;
+                s += "\nAdded b to result";
+                r += b;
+                s += "\nAdded b to result";
+                r /= a - .5 * b;
+                s += "\nDivided the result by the difference of a and half of b";
+                r *= 4 - a;
+                s += "\nMultiplied the result by 4 minus a";
+                r = Math.Round(r, 2);
+                s += "\nRounded to 2 decimals";
 
-            var rv = new KeyValuePair<string, double>(s, r);
-            return rv;
-         }
+                var rv = new KeyValuePair<string, double>(s, r);
+                return rv;
+            }
 
-         return null;
-      }
+            return null;
+        }
 
-   }
+    }
 }
